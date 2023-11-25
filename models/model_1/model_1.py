0cc14341f77455c8e24ec594bade04ee69876d5c
ruslan-DS <ruslanvolo9@gmail.com>
2023-11-25 18:31:54 +0300

    Project completed

--- models/model_1/model_1.py
+++ models/model_1/model_1.py
@@ -7,4 +7,4 @@ DEVICE = 'cpu'
 model = resnet34()
 model.fc = nn.Linear(512, 11)
 
-model.load_state_dict(torch.load('best_params_resnet34.pt', map_location=torch.device(DEVICE)))
+model.load_state_dict(torch.load('../../best_params_resnet34.pt', map_location=torch.device(DEVICE)))
