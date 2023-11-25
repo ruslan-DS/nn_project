656ec9141e7c0a7368e6ba13062f6255eeb21e10
Руслан <146819015+ruslan-DS@users.noreply.github.com>
2023-11-25 18:33:38 +0300

    Update model_2.py

--- models/model_2/model_2.py
+++ models/model_2/model_2.py
@@ -9,5 +9,5 @@ model_2 = densenet121()
 model_2.classifier = nn.Linear(1024, 612)
 model_2.classifier_out = nn.Linear(612, 200)
 
-model_2.load_state_dict(torch.load('../../bestweights_for_bird.pt', map_location=torch.device(DEVICE)))
+model_2.load_state_dict(torch.load('bestweights_for_bird.pt', map_location=torch.device(DEVICE)))
 
